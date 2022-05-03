10172fda5164add7d4ced7fdfee93d6387a8dd10
Jesus Pablo Ramirez Villa <58535306+PabloVilla1999@users.noreply.github.com>
2022-05-03 16:30:29 -0700

    cambios

---
+++ src/components/catalogo.component.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+export default class Catalogo extends Component {
+  render() {
+    return (
+        <React.Fragment>
+            <p style={{color: "white", textAlign:"center"}}>Hola soy el Catalogo</p>
+        </React.Fragment>
+    );
+  }
+}
--- src/components/plataforma.component.js
+++ src/components/plataforma.component.js
@@ -4,52 +4,84 @@ import nintendo from '../assets/nintendo.jpeg';
 import playstation from '../assets/playstation.jpeg';
 import steam from '../assets/steam.jpeg';
 import xgod from '../assets/xgod.jpeg';
+import { Link } from "react-router-dom";
+import {Row, Col} from 'reactstrap';
 
 export default class Plataform extends Component {
   render() {
     return (
-      <div style={{ backgroundColor: "#101648" }}>
+      <div className="plataforma-selection">
+        <br/>
         {/* <Helmet>
         <style>{'body { background-color: red; }'}</style>
       </Helmet> */}
-        <p style={{ textAlign: "center", color: "white", fontSize: "30px" }}>
-          POR FAVOR SELECCIONA LA PLATAFORMA DE TU PREFERENCIA
+        <p style={{ textAlign: "center", color: "white", fontSize: "30px"}}>
+          <strong>SELECCIONA LA PLATAFORMA DE TU PREFERENCIA</strong>
         </p>
-        <br></br>
-        <br></br>
-        <div className="row">
-          <img
-            src={nintendo}
-            style={{ margin: 10, cursor: "pointer" }}
-            alt="Nintendo"
-            width={"455"}
-            height={"255px"}
-          />
-          <img
-            src={playstation}
-            style={{ margin: 10, cursor: "pointer" }}
-            alt="Playstation"
-            width={"455"}
-            height={"255px"}
-          />
+          <Link
+            type="submit"
+            to="../sign-in"
+            style={{ margin: 10, cursor: "pointer", boxShadow: "4px 14px 80px black", borderRadius: 25}}
+            className="zoom"
+            // onMouseEnter={{fontSize:"150%"}}
+          >
+            <img
+              className="img-fluid"
+              src={nintendo}
+              alt="Nintendo"
+              style={{borderRadius: 25}}
+            // width={"320px"}
+            // height={"180px"}
+            />
+          </Link>
+          <Link
+            type="submit"
+            to="../sign-in"
+            style={{ margin: 10, cursor: "pointer", boxShadow: "4px 14px 80px black", borderRadius: 25 }}
+            className="zoom"
+          >
+            <img
+              className="img-fluid"
+              src={playstation}
+              alt="Playstation"
+              style={{borderRadius: 25}}
+            // width={"320px"}
+            // height={"180px"}
+            />
+          </Link>
           {/* </div>
       <div className='row'> */}
-          <img
-            src={steam}
-            style={{ margin: 10, cursor: "pointer" }}
-            alt="Steam"
-            width={"455px"}
-            height={"255px"}
-          />
-          <img
-            src={xgod}
-            style={{ margin: 10, cursor: "pointer" }}
-            alt="Xbox"
-            width={"455px"}
-            height={"255px"}
-          />
+          <Link
+            type="submit"
+            to="../sign-in"
+            style={{ margin: 10, cursor: "pointer", boxShadow: "4px 14px 80px black", borderRadius: 25 }}
+            className="zoom"
+          >
+            <img
+              className="img-fluid"
+              src={steam}
+              alt="Steam"
+              style={{borderRadius: 25}}
+            // width={"320px"}
+            // height={"180px"}
+            />
+          </Link>
+          <Link
+            type="submit"
+            to="../sign-in"
+            style={{ margin: 10, cursor: "pointer", boxShadow: "4px 14px 80px black", borderRadius: 25 }}
+            className="zoom"
+          >
+            <img
+              className="img-fluid"
+              src={xgod}
+              alt="Xbox"
+              style={{borderRadius: 25}}
+            // width={"320px"}
+            // height={"180px"}
+            />
+          </Link>
         </div>
-      </div>
     );
   }
 }
