0f2b4338082b93db65773ad84156f22daad57e1e
LuisDeJesus <luis.de18@tectijuana.edu.mx>
2022-05-30 23:18:08 -0700

    Actualice el historial de comrpas, coloque datos estáticos y cambie compras.css por historial.css

--- src/components2/Historial.js
+++ src/components2/Historial.js
@@ -1,115 +1,59 @@
 import React from 'react';
 import BarraBusqueda from "./barraBusqueda"; 
-import "../css/compras.css"
+import "../css/historial.css"
 import nintendo from "../assets/nintendo.jpeg"
 
 const Historial = () => {
 
    return (
      <div>
-       <h1>Historial de compras</h1>
+       <h1 >Historial de compras</h1>
        <div className='card-wrap'>
          <article className='card'>
            <header className='header-card'>
-             <img src={nintendo}/>
+             <p><b>28 de mayo</b></p>
            </header>
            <footer className='footer-card'>
-             <div className='categoria-card'>
-               <span>Categoria</span>
+             <div className='imagen-card'>
+               <img src="https://cdn-prod.scalefast.com/public/assets/user/122595/image/cdb3282186e2abc6d0b0f610e9ec39c2.png" alt="Elden ring" />
              </div>
-             <div className='texto-card'>
-               <h3>Hola mundo titulo</h3>
-               <p>Hola mundo como estas, esto unicamente es relleno,
-                 creo que ni lo voy a usar, aqui iran los datos de la
-                 compra.
-               </p>
-             </div>
-             <div className='perfil-card'>
-               <div className='img-perfil-card'>
-                 <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSefsPjZCPtkTdmq5XFCmJXLflZZ4lQZvlsCQ&usqp=CAU"/>
-               </div>
-               <div className='text-perfil-card'>
-                 <span>Nombre</span>
-                 <span>Fecha</span>
-               </div>
+             <div className='datos-card'>
+               <h4>Elden Ring</h4>
+               <p className='precio'><b>$1,500.00</b></p>
+               <p>ELDEN RING, desarrollado por FromSoftware, Inc. y BANDAI NAMCO Entertainment Inc. es un RPG de fantasía, acción y aventura ambientado en un mundo creado por Hidetaka Miyazaki y George R. R. Martin. El peligro y el descubrimiento están en cada giro del juego más grande de FromSoftware hasta la fecha.</p>
+               <p>AASAS343434JKDJS3434</p>
              </div>
            </footer>
          </article>
          <article className='card'>
            <header className='header-card'>
-             <img src={nintendo}/>
+             <p><b>27 de mayo</b></p>
            </header>
            <footer className='footer-card'>
-             <div className='categoria-card'>
-               <span>Categoria</span>
+             <div className='imagen-card'>
+               <img src="https://media.vandal.net/m/86451/spiderman-miles-morales-20207921133921_1.jpg" alt="Spiderman Miles Morales" />
              </div>
-             <div className='texto-card'>
-               <h3>Hola mundo titulo</h3>
-               <p>Hola mundo como estas, esto unicamente es relleno,
-                 creo que ni lo voy a usar, aqui iran los datos de la
-                 compra.
-               </p>
-             </div>
-             <div className='perfil-card'>
-               <div className='img-perfil-card'>
-                 <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSefsPjZCPtkTdmq5XFCmJXLflZZ4lQZvlsCQ&usqp=CAU"/>
-               </div>
-               <div className='text-perfil-card'>
-                 <span>Nombre</span>
-                 <span>Fecha</span>
-               </div>
+             <div className='datos-card'>
+               <h4>Spiderman Miles Morales</h4>
+               <p className='precio'><b>$1,200.00</b></p>
+               <p>En la última aventura del universo de Spider-Man de Marvel, el adolescente Miles Morales intenta ajustarse a su nuevo hogar mientras que sigue los pasos de su mentor, Peter Parker, para convertirse en el nuevo Spider-Man.</p>
+               <p>AAAA3343444JFFJS3434</p>
              </div>
            </footer>
          </article>
          <article className='card'>
            <header className='header-card'>
-             <img src={nintendo}/>
+             <p><b>26 de mayo</b></p>
            </header>
            <footer className='footer-card'>
-             <div className='categoria-card'>
-               <span>Categoria</span>
+             <div className='imagen-card'>
+               <img src="https://s2.gaming-cdn.com/images/products/6760/orig-fallback-v1/dragon-ball-z-kakarot-xbox-one-xbox-series-x-s-xbox-one-xbox-series-x-s-juego-microsoft-store-europe-cover.jpg" alt="dragon ball z kakarot" />
              </div>
-             <div className='texto-card'>
-               <h3>Hola mundo titulo</h3>
-               <p>Hola mundo como estas, esto unicamente es relleno,
-                 creo que ni lo voy a usar, aqui iran los datos de la
-                 compra.
-               </p>
-             </div>
-             <div className='perfil-card'>
-               <div className='img-perfil-card'>
-                 <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSefsPjZCPtkTdmq5XFCmJXLflZZ4lQZvlsCQ&usqp=CAU"/>
-               </div>
-               <div className='text-perfil-card'>
-                 <span>Nombre</span>
-                 <span>Fecha</span>
-               </div>
-             </div>
-           </footer>
-         </article>
-         <article className='card'>
-           <header className='header-card'>
-             <img src={nintendo}/>
-           </header>
-           <footer className='footer-card'>
-             <div className='categoria-card'>
-               <span>Categoria</span>
-             </div>
-             <div className='texto-card'>
-               <h3>Hola mundo titulo</h3>
-               <p>Hola mundo como estas, esto unicamente es relleno,
-                 creo que ni lo voy a usar, aqui iran los datos de la
-                 compra.
-               </p>
-             </div>
-             <div className='perfil-card'>
-               <div className='img-perfil-card'>
-                 <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSefsPjZCPtkTdmq5XFCmJXLflZZ4lQZvlsCQ&usqp=CAU"/>
-               </div>
-               <div className='text-perfil-card'>
-                 <span>Nombre</span>
-                 <span>Fecha</span>
-               </div>
+             <div className='datos-card'>
+               <h4>Dragon Ball Z: Kakarot</h4>
+               <p className='precio'><b>$1,500.00</b></p>
+               <p>Disfruta de la historia de DRAGON BALL Z, desde acontecimientos épicos hasta ligeras misiones paralelas divertidas, incluyendo momentos de la historia jamás vistos que responden a algunas preguntas candentes de la tradición de DRAGON BALL por primera vez.</p>
+               <p>H14JTSK78DKSJS78SMSU</p>
              </div>
            </footer>
          </article>
