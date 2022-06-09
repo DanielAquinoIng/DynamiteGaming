0d8b7f95ab16904f29e8f5215832c94dbc4042b1
PabloBluAgent <jesus.ramirezv18@tectijuana.edu.mx>
2022-06-08 17:20:34 -0700

    Galeria

--- src/components2/Juego.js
+++ src/components2/Juego.js
@@ -7,6 +7,14 @@ import '../css/plataforma.css';
 import Button from '@mui/material/Button';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 
+import { useTheme } from '@mui/material/styles';
+import MobileStepper from '@mui/material/MobileStepper';
+import Paper from '@mui/material/Paper';
+import KeyboardArrowLeft from '@mui/icons-material/KeyboardArrowLeft';
+import KeyboardArrowRight from '@mui/icons-material/KeyboardArrowRight';
+import SwipeableViews from 'react-swipeable-views';
+import { autoPlay } from 'react-swipeable-views-utils';
+
 const style = {
   position: 'absolute',
   top: '50%',
@@ -21,7 +29,48 @@ const style = {
   textAlign:"center",
 };
 
+const images = [
+  {
+    label: 'San Francisco – Oakland Bay Bridge, United States',
+    imgPath:
+      'https://images.unsplash.com/photo-1537944434965-cf4679d1a598?auto=format&fit=crop&w=400&h=250&q=60',
+  },
+  {
+    label: 'Bird',
+    imgPath:
+      'https://images.unsplash.com/photo-1538032746644-0212e812a9e7?auto=format&fit=crop&w=400&h=250&q=60',
+  },
+  {
+    label: 'Bali, Indonesia',
+    imgPath:
+      'https://images.unsplash.com/photo-1537996194471-e657df975ab4?auto=format&fit=crop&w=400&h=250&q=80',
+  },
+  {
+    label: 'Goč, Serbia',
+    imgPath:
+      'https://images.unsplash.com/photo-1512341689857-198e7e2f3ca8?auto=format&fit=crop&w=400&h=250&q=60',
+  },
+];
+
 const Juego=({ juego })=> {
+  const AutoPlaySwipeableViews = autoPlay(SwipeableViews);
+
+    const theme = useTheme();
+    const [activeStep, setActiveStep] = React.useState(0);
+    const maxSteps = images.length;
+  
+    const handleNext = () => {
+      setActiveStep((prevActiveStep) => prevActiveStep + 1);
+    };
+  
+    const handleBack = () => {
+      setActiveStep((prevActiveStep) => prevActiveStep - 1);
+    };
+  
+    const handleStepChange = (step) => {
+      setActiveStep(step);
+    };
+
   const [modal, setModal] = useState(false);
   const handleToggle = () => setModal(!modal);
   console.log("Que pro>>>>>>>>>", juego);
@@ -71,20 +120,92 @@ const Juego=({ juego })=> {
           variant="h5" 
           component="h2"
           >
-            {juego.NombreJuego}
+            <strong>{juego.NombreJuego}</strong>
 {/* 
             <button onClick={handleToggle}>x</button> */}
           </Typography>
           <Typography 
           id="modal-modal-description" 
           sx={{ mt: 2 }}
-          style={{marginLeft:"5%", marginRight:"5%"}}
+          style={{marginLeft:"10%", marginRight:"10%", fontSize:"large"}}
           textAlign="justify"
           >
             <strong>Descripcion del Juego</strong>
             <br/>
             {juego.descripcion}
           </Typography>
+          <br></br>
+            <Box sx={{ maxWidth: 400, flexGrow: 1, margin:"auto" }}>
+              <Paper
+              square
+              elevation={0}
+              sx={{
+                display: "flex",
+                alignItems: "center",
+                height: 50,
+                pl: 2,
+                bgcolor: "background.default",
+              }}
+            >
+              <Typography><strong>Galeria</strong></Typography>
+              {/* <Typography>{images[activeStep].label}</Typography> */}
+            </Paper>
+            <AutoPlaySwipeableViews
+              axis={theme.direction === "rtl" ? "x-reverse" : "x"}
+              index={activeStep}
+              onChangeIndex={handleStepChange}
+              enableMouseEvents
+            >
+              {images.map((step, index) => (
+                <div key={step.label}>
+                  {Math.abs(activeStep - index) <= 2 ? (
+                    <Box
+                      component="img"
+                      sx={{
+                        height: 255,
+                        display: "block",
+                        maxWidth: 400,
+                        overflow: "hidden",
+                        width: "100%",
+                      }}
+                      src={step.imgPath}
+                      alt={step.label}
+                    />
+                  ) : null}
+                </div>
+              ))}
+            </AutoPlaySwipeableViews>
+            <MobileStepper
+              steps={maxSteps}
+              position="static"
+              activeStep={activeStep}
+              nextButton={
+                <Button
+                  size="small"
+                  onClick={handleNext}
+                  disabled={activeStep === maxSteps - 1}
+                >
+                  Next
+                  {theme.direction === "rtl" ? (
+                    <KeyboardArrowLeft />
+                  ) : (
+                    <KeyboardArrowRight />
+                  )}
+                </Button>
+              }
+              backButton={
+                <Button size="small" onClick={handleBack} disabled={activeStep === 0}>
+                  {theme.direction === "rtl" ? (
+                    <KeyboardArrowRight />
+                  ) : (
+                    <KeyboardArrowLeft />
+                  )}
+                  Back
+                </Button>
+              }
+            />
+          </Box>
+          <br></br>
           <Button variant="contained" href="">
             <ShoppingCartIcon/>
             Comprar
